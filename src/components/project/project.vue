cfac37cf1a8084d4d0dd2cc4c79c406503950569
PStarH <sampan090611@gmail.com>
2024-11-27 18:25:40 +0800

    commit

--- src/components/project/project.vue
+++ src/components/project/project.vue
@@ -1,10 +1,79 @@
-<script setup>
+<script setup lang="ts">
+import { projects, projectData } from './project';
+import { NBreadcrumb, NBreadcrumbItem, NLayout, NLayoutContent, NLayoutHeader, NGrid, NGi, NImage, NStatistic, NText, NRow, NCol } from 'naive-ui';
+import router from "../../router/router"
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const project = ref<projectData | null>(null);
+
+// Define the route using Composition API
+const route = useRoute();
+
+// Create a computed property for route name
+const routeName = computed(() => route.name);
+
+initial();
+
+function initial() {
+    project.value = projects.find(project => project.routerKey == router.currentRoute.value.name)!
+}
 </script>
 
 <template>
     <div>
-        <p>This might be your Project page.</p>
+        <div v-if="(router.currentRoute.value.name as string).split('-')[1]"
+            style="padding: 0 24px; margin: 0 auto">
+            <n-breadcrumb>
+                <n-breadcrumb-item @click="router.push({ name: 'project' })"> Project
+                </n-breadcrumb-item>
+                <n-breadcrumb-item> {{ project.name }}
+                </n-breadcrumb-item>
+            </n-breadcrumb>
+        </div>
+        <router-view />
     </div>
 </template>
 
-<style scoped></style>
+<style>
+.lout {
+    padding-top: 10px;
+    padding-right: 100px;
+    padding-left: 100px;
+}
+
+.subHead {
+    font-size: 1.5rem;
+    font-weight: bold;
+    padding-bottom: 5px;
+}
+
+.projectsCard {
+    border-radius: 12px;
+    padding: 24px;
+    cursor: pointer;
+    transition: all 0.2s;
+    box-shadow: var(--boxShadow-light);
+
+    &:hover {
+        box-shadow: var(--boxShadow-regular)
+    }
+}
+
+.tag {
+    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: white;
+    font-weight: bold;
+    margin: 5px;
+}
+
+.bfs {
+    background-color: rgba(13, 52, 249, 0.5);
+}
+
+.binary-search {
+    background-color: rgb(66, 248, 15, 0.5);
+}
+</style>
--- src/store/modules/display/display.js
+++
@@ -1,7 +0,0 @@
-export const store = {
-    state: () => ({
-        isMobile: false,
-        isLoading: false,
-        showAuth: false,
-    }),
-}
